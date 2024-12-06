060a881143af73737b92b8aba7761536eac8e22c
ben3345678 <q20038848@gmail.com>
2024-12-06 14:51:44 +0800

    feat: Develop Test

--- apps/project-a/src/App.vue
+++ apps/project-a/src/App.vue
@@ -6,7 +6,7 @@ import { Button as AButton } from 'ant-design-vue'
 </script>
 
 <template>
-  <Button>Deploy Github Page Test222</Button>
+  <Button>Deploy Github Page Test Develop</Button>
   <a-button class="bg-red-500 border-red-500 text-white">Antd Button</a-button>
   <p class="text-white p-4 m-4 underline text-lg">in this project test</p>
   <RouterView />
