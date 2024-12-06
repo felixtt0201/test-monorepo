3555a3c6d8f5e960847ecdf9b22af40be572bcb7
ben3345678 <q20038848@gmail.com>
2024-12-06 14:17:38 +0800

    feat: 測試自動部署

--- apps/project-a/src/App.vue
+++ apps/project-a/src/App.vue
@@ -6,8 +6,8 @@ import { Button as AButton } from 'ant-design-vue'
 </script>
 
 <template>
-  <Button>share Button 123</Button>
-  <a-button class="bg-blue-500 border-blue-500 text-white">Antd Button</a-button>
+  <Button>Deploy Test</Button>
+  <a-button class="bg-red-500 border-red-500 text-white">Antd Button</a-button>
   <p class="text-white p-4 m-4 underline text-lg">in this project test</p>
   <RouterView />
 </template>
